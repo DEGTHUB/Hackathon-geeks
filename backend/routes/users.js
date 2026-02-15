cada9aa2684e0aeefefc2f9d0b00366b242d8e48
DEGTHUB <debasishdas3804@gmail.com>
2026-02-15 13:20:01 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,48 +11,143 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 
 
-const studentSchema = new mongoose.Schema({
+//------------------------------------client----------------------------------------------------
+
+
+const clientSchema = new mongoose.Schema({
+
   name: { type: String, required: true },
-  username:{ type:String, required:true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
+
+  cases: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Case"
+  }],
+
+  connectedLawyers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lawyer"
+  }],
+
+
   points: { type:Number, required:true, default:0},
   level: { type: String, default:"Bronze" },
   completed:{ type:Number, required:true, default:0},
+
+
+ 
   isverified:{ type:Boolean, default:false},
-  institution: { type: mongoose.Schema.Types.ObjectId, ref: "Institution" },
   otp: { type: String },
 
+
 });
 
-studentSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+clientSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
-const parentSchema = new mongoose.Schema({
+
+//------------------------------------------------------------------------------------------
+
+
+
+const lawyerSchema = new mongoose.Schema({
+
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
+
+
+  phone: String,
+
+  specialization: [String],
+
+  experienceYears: Number,
+
+  barRegistrationNumber: {
+    type: String,
+    required: true
+  },
+
+  linkedinProfileid: Number,
+
+
+
+    cases: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Case"
+  }],
+
+
+
   otp: {type: String},
   isverified:{type: Boolean, default:false},
-  children: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }], 
-});
-parentSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
-const teacherSchema = new mongoose.Schema({
+
+
+
+  
+});
+
+lawyerSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+
+
+
+
+//---------------------------------------------------JUDGES------------------------------------
+
+
+const judgeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  otp: {type:String},
-  institution: { type: mongoose.Schema.Types.ObjectId, ref: "Institution" },
+  
+
+
+   courtName: {
+    type: String,
+    required: true
+  },
+
+   barRegistrationNumber: {
+    type: String,
+    required: true
+  },
+
+  designation: String,
+
+  assignedCases: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Case"
+  }],
+
+  judgementsGiven: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Judgement"
+  }],
+
+
+
+
+  otp: {type: String},
+  isverified:{type: Boolean, default:false},
+
+
 });
 
-teacherSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+judgeSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
-const institutionSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  code:{type:String, required:true }, 
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
-  teachers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Teacher" }]
-});
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+//-------------------------------------admin---------------------------------------------------------------
 
 const adminSchema = new mongoose.Schema({
   
@@ -63,15 +158,85 @@ const adminSchema = new mongoose.Schema({
  
 });
 
-const Student = mongoose.model("Student", studentSchema);
-const Parent = mongoose.model("Parent", parentSchema);
-const Teacher = mongoose.model("Teacher", teacherSchema);
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+const caseSchema = new mongoose.Schema({
+
+  caseTitle: {
+    type: String,
+    required: true
+  },
+
+  caseNumber: {
+    type: String,
+    unique: true
+  },
+
+  description: String,
+
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client"
+  },
+
+  lawyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lawyer"
+  },
+
+  judge: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Judge"
+  },
+
+  status: {
+    type: String,
+    enum: ["filed", "in-progress", "hearing", "closed"],
+    default: "filed"
+  }
+
+}, { timestamps: true });
+
+
+//--------------------------------------------------------------------------------------------
+
+
+
+const judgementSchema = new mongoose.Schema({
+
+  case: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Case"
+  },
+
+  judge: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Judge"
+  },
+
+  fullVerdict: String,
+
+  simplifiedVerdict: String
+
+}, { timestamps: true });
+
+
+
+
+const Client = mongoose.model("Client", clientSchema);
+const Lawyer = mongoose.model("Lawyer", lawyerSchema);
+const Judge = mongoose.model("Judge", judgeSchema);
 const Admin = mongoose.model("Admin", adminSchema);
-const Institution = mongoose.model("Institution", institutionSchema);
+const Case =  mongoose.model("Case", caseSchema);
+const Judgement=mongoose.model("Judgement", judgementSchema);
 
 
 
-module.exports = { Student, Parent, Teacher, Institution, Admin };
+
+
+module.exports = { Client, Lawyer, Judge, Admin, Case , Judgement};
 
 
 
